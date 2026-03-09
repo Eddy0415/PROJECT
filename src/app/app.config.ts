dfeadc8d65ed326be6798d66b6de6b31adbd9e38
Eddy <edmond.cham@outlook.com>
2026-03-09 15:36:55 +0200

    added zoneless change detection to app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core'; // app config type
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core'; // app config type
 import { provideRouter } from '@angular/router'; // router provider
 import { provideHttpClient, withInterceptors } from '@angular/common/http'; // http provider
 import { routes } from './app.routes'; // routes
@@ -6,6 +6,7 @@ import { authInterceptor } from './core/auth/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZonelessChangeDetection(),
     provideRouter(routes), // enable routing
     provideHttpClient(withInterceptors([authInterceptor])), // enable HttpClient
   ],
